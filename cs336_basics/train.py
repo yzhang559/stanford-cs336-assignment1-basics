b6e5bddc2e11b29eb2e7e0a662ab6a742cc84e59
Yu Zhang <yzhang12@linkedin.com>
2026-02-24 20:54:06 -0800

    5.2 checkpointing done

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -1,3 +1,6 @@
+import os
+from typing import Union, BinaryIO
+
 import torch
 import numpy as np
 
@@ -12,3 +15,26 @@ def data_loading(data: np.ndarray, batch_size: int, context_length: int, device:
 
     return torch.from_numpy(inputs).to(dtype=torch.long, device=device), \
         torch.from_numpy(targets).to(dtype=torch.long, device=device)
+
+
+def save_checkpoint(model: torch.nn.Module, optimizer: torch.optim.Optimizer, iteration: int,
+                    out: Union[str, os.PathLike, BinaryIO]):
+    torch.save({
+        "iteration": iteration,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+    }, out)
+
+    if isinstance(out, (str, os.PathLike)):
+        print(f"Saved checkpoint to {out}")
+
+
+def load_checkpoint(src: Union[str, os.PathLike, BinaryIO], model: torch.nn.Module, optimizer: torch.optim.Optimizer):
+    state = torch.load(src)
+    model.load_state_dict(state["model_state_dict"])
+    optimizer.load_state_dict(state["optimizer_state_dict"])
+
+    if isinstance(src, (str, os.PathLike)):
+        print(f"Loaded checkpoint from {src} at iteration {state['iteration']}")
+
+    return state["iteration"]
--- tests/adapters.py
+++ tests/adapters.py
@@ -602,8 +602,8 @@ def run_save_checkpoint(
             we've completed.
         out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
-
+    from cs336_basics.train import save_checkpoint
+    save_checkpoint(model, optimizer, iteration, out)
 
 def run_load_checkpoint(
     src: str | os.PathLike | BinaryIO | IO[bytes],
@@ -623,7 +623,8 @@ def run_load_checkpoint(
     Returns:
         int: the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    from cs336_basics.train import load_checkpoint
+    return load_checkpoint(src, model, optimizer)
 
 
 def get_tokenizer(
